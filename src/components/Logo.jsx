359bfda7f35dd930f04deb12667e7bf8b96d1fb3
Mikiyas6 <Mikitedi9@gmail.com>
2024-12-18 00:06:58 +0300

    final commit

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
 import styles from "./Logo.module.css";
-import logo from "../assets/logo.png";
 function Logo() {
   return (
     <Link to="/">
-      <img src={logo} alt="WorldWise logo" className={styles.logo} />{" "}
+      <img
+        src="/public/logo.png"
+        alt="WorldWise logo"
+        className={styles.logo}
+      />{" "}
     </Link>
   );
 }
